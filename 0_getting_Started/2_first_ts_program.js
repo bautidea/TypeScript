2f591f7f2c4c8402f0f7f58991d1638bbdcad3fb
bautidea <juanbautistadeangelis@gmail.com>
2023-10-07 11:24:18 -0300

    Finish.

---
+++ 0_getting_Started/2_first_ts_program.js
@@ -0,0 +1,7 @@
+// We created this file by running in terminal --> tsc 2_first_ts_program.ts
+
+// instead of the 'let' keyword we have 'var', because by default the TS compiler uses
+// an older version of JS ES5.
+var age = 20;
+
+// In the next lesson we are going to see how to compile TS code to a newer version of JS.
---
+++ 0_getting_Started/2_first_ts_program.ts
@@ -0,0 +1,7 @@
+// To compile our program, we open the terminal and run the command
+// * tsc 2_first_ts_program.ts
+// This will generate a new JavaScript file
+
+// To declare a variable we use the 'let' keyword, then the name of the variable, and by typing
+// ':' we can annotate or explain a variable.
+let age: number = 20;
