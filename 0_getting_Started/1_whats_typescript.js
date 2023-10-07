1dd9cc8ca0c992d5d4f301ff9c03425f86884ed6
bautidea <juanbautistadeangelis@gmail.com>
2023-10-07 10:58:51 -0300

    Starting the course.

---
+++ 0_getting_Started/1_whats_typescript.js
@@ -0,0 +1,30 @@
+// TypeScript is a programming language to address defects of JS
+// TS is build on top of JS so every JS file is a valid TypeScript file.
+// TS added some really useful features that help us build more robust and maintainable applications in less time
+// Benefits:
+
+//  - Static typing -->  We know the type of variables at compile time or while coding
+//      For example we decalare a variable of type int
+// *        lex x: number = 10; --> This variable can only hold int values.
+//      If we declare a variable as a number we cannot set it to a string //// x = 'a'
+//      In Dynamic typed langagues we can change the type of variables at runtime (JS is dynamic).
+// !    TypeScript is essentially JS with Type Checking. With TS we explicitly set the type of variables upon declaration.
+
+// Most code editors have great support for TS, so they can detect the type of variables and offer features to write cleaner and concise code:
+//  - Code Completion
+//  - Refactoring
+//  - Shorthand notations
+
+// These features get added to JS, but because runtime environments and various browsers for executing JS code it takes time
+// until these features are implemented in various browsers.
+// By coding in TS we can use the features of future JS.
+
+// So TS is build intop of JS and we can use it whenever we use Js on Front or Back-end, everything we do in JS can be done with TS.
+
+// The drawbacks of TS are:
+//  -   Compilation --> Theres always a compilation step involved because browsers dont understand TS code.
+//          So we have to give our code to a JS compiler, to compile and transfer into JS This prosses is callded // * Transpilation.
+//  - Disipline in coding --> We have to be more careful when writing code.
+
+// TypeScript is used in Medium to large projects.
+// JavaScript is used in Simple projects.
