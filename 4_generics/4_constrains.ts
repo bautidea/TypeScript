97cb720a122a8fab2f6a7d3dff8affa604156d6a
bautidea <juanbautistadeangelis@gmail.com>
2023-10-24 16:10:46 -0300

    Finish.

--- 4_generics/4_constrains.ts
+++ 4_generics/4_constrains.ts
@@ -7,9 +7,44 @@ function echo<T>(value: T): T {
 // Now we can call the function an pass any value that we want.
 echo('hi');
 echo(2);
-//  But if we want to constrain or limit the type of objects we can pass here?
-// Right after the generic type parameter we use the 'extends' keyword and then we specify the type 
-// cn be assigned
-function echo2<T extends>(value: T): T {
-    return value;
-  }
+
+// But if we want to constrain or limit the type of objects we can pass here?
+// Right after the generic type parameter we use the 'extends' keyword and then we specify the type
+// can be assigned to 'T'.
+// In this function we can only pass number or string types.
+function echo2<T extends number | string>(value: T): T {
+  return value;
+}
+
+echo2(true); // --> compilation error.
+
+// We can pass the shape of an object.
+function echo3<T extends { name: string }>(value: T): T {
+  return value;
+}
+
+echo3({ name: 'a' });
+
+// We can also constrain by an interface.
+interface InterfPerson {
+  name: string;
+}
+function echo4<T extends InterfPerson>(value: T): T {
+  return value;
+}
+
+// We can constrain by class.
+class ClassPerson {
+  constructor(public name: string) {}
+}
+function echo5<T extends ClassPerson>(value: T): T {
+  return value;
+}
+// Now we can pass an instace of that class or any classes that derives from ClassPerson.
+class Customer extends ClassPerson {
+  // Here im not going to add any extra logic is just to show that when calling echo
+  // we can pass this class that directly derives from Person.
+}
+
+echo5(new ClassPerson('Bauti'));
+echo5(new Customer('Jiu'));
